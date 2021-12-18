1d6d307116966db59c48437b77360fcfa71f96ba
Nestor <elnestinatorHD@gmail.com>
2021-12-18 17:56:35 +0100

    Make main fn async and start api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
-    let hello = "Hello world";
-    println!("{}", hello);
+mod server;
+
+extern crate log;
+extern crate pretty_env_logger;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    server::start(([127, 0, 0, 1], 3030)).await;
 }
